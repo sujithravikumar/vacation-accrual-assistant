26ee77399cc47c4904e5512103c1c0ad9c29b3d8
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-03-03 14:19:58 -0800

    Add welcome logic and user information retrieval logic

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const { Alexa } = require('jovo-platform-alexa');
 const { GoogleAssistant } = require('jovo-platform-googleassistant');
 const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
+const rp = require('request-promise');
 
 const app = new App();
 
@@ -26,11 +27,41 @@ app.use(
 
 app.setHandler({
     LAUNCH() {
-        this.ask('Hey John! Would you like to know your vacation balance?', 'Would you like to know your vacation balance?');
+        if (!this.$request.getAccessToken()){
+            if (this.isAlexaSkill()) {
+                this.tell('You must authenticate with your Amazon Account to use this skill');
+            }
+            else if (this.isGoogleAction()) {
+                this.tell('You must sign in to use this Action');
+            }
+        }
+        else {
+            this.ask('Welcome to Vacation Buddy! Would you like to know your vacation balance?', 'Would you like to know your vacation balance?');
+        }
     },
 
-    BalanceIntent() {
-        this.tell('John, your vacation balance for the current pay period is 100 hours.');
+    async BalanceIntent() {
+        let data;
+        if (this.isAlexaSkill()) {
+            let url = `https://api.amazon.com/user/profile?access_token=${this.$request.getAccessToken()}`;
+    
+            let response = await rp(url);
+            data = JSON.parse(response);
+        }
+        else if (this.isGoogleAction()) {
+            let token = this.$request.getAccessToken();
+            let options = {
+                method: 'GET',
+                uri: 'https://vacationaccrualbuddy.auth0.com/userinfo',
+                headers: {
+                    authorization: 'Bearer ' + token,
+                }
+            };
+
+            let response = await rp(options);
+            data = JSON.parse(response);
+        }
+        this.tell(data.name + ', your vacation balance for the current pay period is 100 hours.');
     },
 
     'AMAZON.YesIntent'() {
